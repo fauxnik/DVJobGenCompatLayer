6f99ec6ae6f3610df9a8f07a2d98483d0cf780cb
Niko Fox <niko@fauxnik.me>
2023-02-05 23:49:25 -0800

    add mod settings

--- JobGenCompatLayer/Mod.cs
+++ JobGenCompatLayer/Mod.cs
@@ -1,16 +1,25 @@
-﻿using UnityModManagerNet;
+﻿using System;
+using UnityModManagerNet;
 
 namespace JobGenCompatLayer
 {
     public static class Mod
     {
         public static UnityModManager.ModEntry Entry { get; private set; }
+        public static Settings Settings { get; private set; }
 
         static void OnLoad(UnityModManager.ModEntry loadedEntry)
         {
             Entry = loadedEntry;
 
             if (!Entry.Enabled) { return; }
+
+            try { Settings = Settings.Load<Settings>(Entry); }
+            catch (Exception ex)
+            {
+                Debug.LogWarning(() => $"Using default mod settings. Saved settings failed to load:\n{ex}");
+                Settings = new Settings();
+            }
         }
     }
 }
---
+++ JobGenCompatLayer/Settings.cs
@@ -0,0 +1,17 @@
+﻿using UnityModManagerNet;
+
+namespace JobGenCompatLayer
+{
+    public class Settings : UnityModManager.ModSettings, IDrawable
+    {
+        [Draw("Log level")]
+        public LogLevel selectedLogLevel =
+#if DEBUG
+            LogLevel.Debug;
+#else
+            LogLevel.Warn;
+#endif
+
+        public void OnChange() { }
+    }
+}
