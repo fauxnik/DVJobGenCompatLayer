933c54ecbf411eca809cc8ebfdd025ae55432dd7
Niko Fox <niko@fauxnik.me>
2023-02-05 23:46:49 -0800

    handle mod entry loading

---
+++ JobGenCompatLayer/Mod.cs
@@ -0,0 +1,16 @@
+﻿using UnityModManagerNet;
+
+namespace JobGenCompatLayer
+{
+    public static class Mod
+    {
+        public static UnityModManager.ModEntry Entry { get; private set; }
+
+        static void OnLoad(UnityModManager.ModEntry loadedEntry)
+        {
+            Entry = loadedEntry;
+
+            if (!Entry.Enabled) { return; }
+        }
+    }
+}
